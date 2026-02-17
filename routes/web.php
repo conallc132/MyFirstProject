4a316d9303f51e6bf5a14afa55286b748370bf2b
conallc132 <Curran-C53@ulster.ac.uk>
2026-02-17 10:21:25 +0000

    Updating the web.php file

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
-});
-
-Route::get('/jobs', function () {
-    return view('jobs',  [
-        'jobs' => [
+class Job {
+    public static function all():array
+    {
+        return [
             [    
                 'id' => 1,
                 'title' => 'Manager',
@@ -25,38 +22,27 @@ Route::get('/jobs', function () {
                 'Salary' => '32,000'
             ]
 
-            ]
-        ]);
-    
-});
-
-Route::get('/jobs/{id}', function ($id) {
-    $jobs = [
-        1 => [
-            'id' => 1,
-            'title' => 'Manager',
-            'Salary' => '50,000',
-    
-        ],
-        2 => [
-            'id' => 2,
-            'title' => 'Engineer',
-            'Salary' => '40,000',
             
-        ],
-        3 => [
-            'id' => 3,
-            'title' => 'Technician',
-            'Salary' => '32,000',
-            
-        ]
-
         ];
-    $jobs=\Illuminate\Support\Arr::first($jobs, function ($job) use ($id) {
-        return $job['id'] == $id;
-    });
-    return view('job', ['job' => $jobs]);
+    }
+}
+
+Route::get('/', function () {
+    return view('home');
 });
+
+Route::get('/jobs', function () {
+    return view('jobs',  [
+        'jobs' => Job::all()
+    ]);
+    
+});
+
+Route::get('/jobs/{id}', function ($id) use ($jobs) {
+
+ $jobs=\Illuminate\Support\Arr::first($jobs, fn($job) => $job['id']);
+    return view('job', ['job' => $job]);
+}
 
 Route::get('/contact', function () {
     return view('contact');
